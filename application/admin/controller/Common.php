905abbe72962ff2eb7c7b5958487848c5e7c1ae2
283010247 <283010247@qq.com>
2017-11-26 10:46:42 +0800

    第一阶段完成

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -6,7 +6,7 @@ use think\Request;
 class Common extends Controller{
 	public function __construct(Request $request){
 		parent::__construct($request);
-		if(!session('user_id') || session('user_sign') !== md5('admin')){
+		if(!session('user.user_id') || session('user.user_sign') !== md5('admin')){
 			$this->error('没有权限访问，请先登录后台！','admin/Login/index');
 			exit();
 		}
